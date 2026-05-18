37e7a17b7cfd35d0f7e7eb9d9eb47a42b1242be7
atusy <30277794+atusy@users.noreply.github.com>
2026-05-19 00:08:22 +0900

    chore(build): follow https://code.visualstudio.com/api/working-with-extensions/bundling-extension#using-esbuild

--- esbuild.js
+++ esbuild.js
@@ -1,29 +1,55 @@
-const esbuild = require("esbuild");
+import { context } from "esbuild";
 
 const production = process.argv.includes("--production");
 const watch = process.argv.includes("--watch");
 
-const ctx = {
-  entryPoints: ["src/extension.ts"],
-  bundle: true,
-  format: "cjs",
-  platform: "node",
-  target: "node18",
-  external: ["vscode"],
-  outfile: "dist/extension.js",
-  sourcemap: !production,
-  minify: production,
-  logLevel: "info",
-};
-
 async function main() {
+  const ctx = await context({
+    entryPoints: ["src/extension.ts"],
+    bundle: true,
+    format: "cjs",
+    minify: production,
+    sourcemap: !production,
+    sourcesContent: false,
+    platform: "node",
+    outfile: "dist/extension.js",
+    external: ["vscode"],
+    logLevel: "warning",
+    plugins: [
+      /* add to the end of plugins array */
+      esbuildProblemMatcherPlugin,
+    ],
+  });
   if (watch) {
-    const context = await esbuild.context(ctx);
-    await context.watch();
+    await ctx.watch();
   } else {
-    await esbuild.build(ctx);
+    await ctx.rebuild();
+    await ctx.dispose();
   }
 }
+
+/**
+ * @type {import('esbuild').Plugin}
+ */
+const esbuildProblemMatcherPlugin = {
+  name: "esbuild-problem-matcher",
+
+  setup(build) {
+    build.onStart(() => {
+      console.log("[watch] build started");
+    });
+    build.onEnd((result) => {
+      result.errors.forEach(({ text, location }) => {
+        console.error(`✘ [ERROR] ${text}`);
+        if (location == null) return;
+        console.error(
+          `    ${location.file}:${location.line}:${location.column}:`,
+        );
+      });
+      console.log("[watch] build finished");
+    });
+  },
+};
 
 main().catch((e) => {
   console.error(e);
